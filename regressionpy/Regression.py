8c85566e7a23f458f078c3b98589a6a16f6eff0b
Git-Admin <mark.hewitt@centurionsolutions.com.au>
2018-06-09 21:13:05 +0200

    Process multiple data sets at once

--- regressionpy/Regression.py
+++ regressionpy/Regression.py
@@ -8,6 +8,7 @@ Base on code from https://www.quantinsti.com/blog/gold-price-prediction-using-ma
 """
 
 import sys
+import os
 
 # LinearRegression is a machine learning library for linear regression 
 from sklearn.linear_model import LinearRegression 
@@ -23,78 +24,82 @@ import seaborn
 # fix_yahoo_finance is used to fetch data 
 import fix_yahoo_finance as yf
 
-# get the symbol to process, default to script using H1 EURUSD
-if len(sys.argv) == 1: sys.argv[1:] = ["EURUSD60"]
-symbol = sys.argv[1]
+# get symbols, if a command line arg is given, show only that one with its regression graphs
+# otherwise fetch all CSV files from ../data/rgpy and process them all but showing now regression graph 
+show_regression = False;
+if len(sys.argv) == 1:
+    symbols = list()
+    for filename in os.listdir("../data/rgpy"):
+		if filename.endswith(".csv"): 
+			symbols.append(filename.replace('.csv',''))
+			continue
+		else:
+			continue
+else:
+	symbols = [sys.argv[1]]
+	show_regression = True;
+	
+for symbol in symbols:
+	#print("Using symbol " + symbol)
+	#print("Downloading historical data...");
 
-print("Using symbol " + symbol)
-print("Downloading historical data...");
+	Df = pd.read_csv("../data/rgpy/" + symbol + '.csv', dtype={'Close':np.float32},
+						 names=['Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume'], 
+						 index_col='Date_Time', parse_dates=[[0, 1]])
 
-# Read data 
-#Df = yf.download(symbol,'2012-01-01','2018-06-07')
+	# Only keep close columns 
+	Df=Df[['Close']] 
 
-Df = pd.read_csv(symbol + '.csv', dtype={'Close':np.float32},
-                     names=['Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume'], 
-                     index_col='Date_Time', parse_dates=[[0, 1]])
+	# Drop rows with missing values 
+	Df= Df.dropna() 
 
-# Only keep close columns 
-Df=Df[['Close']] 
+	#Define explanatory variables
+	Df['S_3'] = Df['Close'].shift(1).rolling(window=3).mean() 
+	Df['S_9']= Df['Close'].shift(1).rolling(window=9).mean() 
+	Df= Df.dropna() 
+	X = Df[['S_3','S_9']] 
+	X.head()
 
-# Drop rows with missing values 
-Df= Df.dropna() 
+	#Define dependent variable
+	y = Df['Close']
+	y.head()
 
-# Plot the closing price of GLD 
-#Df.Close.plot(figsize=(10,5)) 
-#plt.ylabel(symbol + " Prices")
-#print (symbol + " Price Series")
-#plt.show()
+	#Split the data into train and test dataset
+	t=.8 
+	t = int(t*len(Df)) 
 
-#Define explanatory variables
-Df['S_3'] = Df['Close'].shift(1).rolling(window=3).mean() 
-Df['S_9']= Df['Close'].shift(1).rolling(window=9).mean() 
-Df= Df.dropna() 
-X = Df[['S_3','S_9']] 
-X.head()
+	# Train dataset 
+	X_train = X[:t] 
+	y_train = y[:t]  
 
-#Define dependent variable
-y = Df['Close']
-y.head()
+	# Test dataset 
+	X_test = X[t:] 
+	y_test = y[t:]
 
-#Split the data into train and test dataset
-t=.8 
-t = int(t*len(Df)) 
+	#Create a linear regression model
+	linear = LinearRegression().fit(X_train,y_train)
+	#print (symbol + " Price (y) =", \
+	#round(linear.coef_[0],2), "* 3 Days Moving Average (x1)", \
+	#round(linear.coef_[1],2), "* 9 Days Moving Average (x2) +", \
+	#round(linear.intercept_,2), "(constant)")
 
-# Train dataset 
-X_train = X[:t] 
-y_train = y[:t]  
+	#Predicting the Gold ETF prices
+	#print ( X_test[len(X_test)-7:])
+	predicted_price = linear.predict(X_test)
+	#print(predicted_price[len(predicted_price)-7:])
+	
+	if show_regression:
+		predicted_price = pd.DataFrame(predicted_price,index=y_test.index,columns = ['price'])  
+		predicted_price.plot(figsize=(10,5))  
+		y_test.plot()  
+		plt.legend(['predicted_price','actual_price'])  
+		plt.ylabel(symbol + " Price")  
+		plt.show()
 
-# Test dataset 
-X_test = X[t:] 
-y_test = y[t:]
-
-#Create a linear regression model
-linear = LinearRegression().fit(X_train,y_train)
-print "Linear Regression equation"
-print (symbol + " Price (y) =", \
-round(linear.coef_[0],2), "* 3 Days Moving Average (x1)", \
-round(linear.coef_[1],2), "* 9 Days Moving Average (x2) +", \
-round(linear.intercept_,2), "(constant)")
-
-#Predicting the Gold ETF prices
-print ( X_test[len(X_test)-7:])
-predicted_price = linear.predict(X_test)
-print(predicted_price[len(predicted_price)-7:])
-
-predicted_price = pd.DataFrame(predicted_price,index=y_test.index,columns = ['price'])  
-predicted_price.plot(figsize=(10,5))  
-y_test.plot()  
-plt.legend(['predicted_price','actual_price'])  
-plt.ylabel(symbol + " Price")  
-plt.show()
-
-#for bar in predicted_price:
-#    print bar
-
-# R square
-r2_score = linear.score(X[t:],y[t:])*100  
-print float("{0:.2f}".format(r2_score))
+	# R square
+	r2_score = linear.score(X[t:],y[t:])*100  
+	if  r2_score < 95 :
+		print symbol + ": R2 Score of " +  float("{0:.2f}".format(r2_score)) + " is too low, symbol rejected"
+	else:	
+		print symbol + ": m1 = " + str(round(linear.coef_[0],2)) + "; m2 = " + str(round(linear.coef_[1],2)) + "; c = " + str(round(linear.intercept_,2)) + "; R2 Score: " + str(float("{0:.2f}".format(r2_score))) + "%"
+	#print float("{0:.2f}".format(r2_score))
