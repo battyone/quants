54ba628ac784d1e1c6752086668b63839c87d996
Git-Admin <mark.hewitt@centurionsolutions.com.au>
2018-06-09 23:14:11 +0200

    Output results sorted best - worst

--- regressionpy/Regression.py
+++ regressionpy/Regression.py
@@ -38,7 +38,8 @@ if len(sys.argv) == 1:
 else:
 	symbols = [sys.argv[1]]
 	show_regression = True;
-	
+
+results = []	
 for symbol in symbols:
 	#print("Using symbol " + symbol)
 	#print("Downloading historical data...");
@@ -98,8 +99,12 @@ for symbol in symbols:
 
 	# R square
 	r2_score = linear.score(X[t:],y[t:])*100  
-	if  r2_score < 95 :
+	
+	results.append([float("{0:.2f}".format(r2_score)),str(round(linear.coef_[0],2)),str(round(linear.coef_[1],2)),str(round(linear.intercept_,2)) ]);
+
+for r in sorted(results, key=lambda result: result[0], reverse=True):	
+	if  r[0] < 95 :
 		print symbol + ": R2 Score of " +  str(float("{0:.2f}".format(r2_score))) + " is too low, symbol rejected"
 	else:	
-		print symbol + ": m1 = " + str(round(linear.coef_[0],2)) + "; m2 = " + str(round(linear.coef_[1],2)) + "; c = " + str(round(linear.intercept_,2)) + "; R2 Score: " + str(float("{0:.2f}".format(r2_score))) + "%"
+		print symbol + ": m1 = " + r[1] + "; m2 = " + r[2] + "; c = " + r[3] + "; R2 Score: " + str(r[0]) + "%"
 	#print float("{0:.2f}".format(r2_score))
